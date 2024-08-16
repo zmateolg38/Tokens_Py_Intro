50ae028607676d9353880d2c11fcc15117b507b1
zmateolg38 <105138915+zmateolg38@users.noreply.github.com>
2024-08-16 16:18:17 +0000

    TokenV2

--- tokenv1.py
+++ tokenv1.py
@@ -53,3 +53,5 @@ if tokens:  # Verifica si hay tokens en la lista
     print("", "".join(map(str, [primer_token,", ",segundo_token])))
 else:
     print("No se encontraron tokens.")
+
+print("teamo")
